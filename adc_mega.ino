88fe16e901eb8375fb07bf9d2d38938d398bd09d
imoe <lym602@126.com>
2017-02-15 18:04:42 +0300

    Using A0 and smoother

--- adc_mega.ino
+++ adc_mega.ino
@@ -1,17 +1,20 @@
 
-int sensorPin = A1;    // select the input pin for the potentiometer
+int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
+int outputValue = 0; 
+float alpha = 0.2; 
 
 void setup() {
   // declare the ledPin as an OUTPUT:
   
-  Serial.begin(57600);
+  Serial.begin(9600);
 }
 
 void loop() {
   // read the value from the sensor:
+  //delay(5); 
   sensorValue = analogRead(sensorPin);
- 
-  Serial.println(sensorValue);
+  outputValue = (1-alpha) * outputValue + alpha * sensorValue; 
+  Serial.println(outputValue);
    
 }
