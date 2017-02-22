41e38514d801b1e1282d27d003a01ac20b575597
imoe <lym602@126.com>
2017-02-22 16:32:13 +0300

    debugged version. changed threshold, time threshold and publisher name

--- adc_mega/adc_mega.ino
+++ adc_mega/adc_mega.ino
@@ -5,9 +5,10 @@
 
 ros::NodeHandle nh; 
 std_msgs::Float32 SICK_msg; 
-ros::Publisher SICK_sensor("SICK_sensor", &SICK_msg); 
+ros::Publisher SICK_time("SICK_time", &SICK_msg); 
 std_msgs::Int32 SICK_debug; 
-ros::Publisher SICK_counter("SICK_counter", &SICK_debug); 
+ros::Publisher SICK_distance("SICK_distance", &SICK_debug); 
+
 
 int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
@@ -20,8 +21,8 @@ bool t0_set_flag = false;
 void setup() {
   // declare the ledPin as an OUTPUT:
   nh.initNode(); 
-  nh.advertise(SICK_sensor); 
-  nh.advertise(SICK_counter);
+  nh.advertise(SICK_time); 
+  nh.advertise(SICK_distance);
   Serial.begin(57600);
 }
 
@@ -31,23 +32,23 @@ void loop() {
   sensorValue = analogRead(sensorPin);
   outputValue = (1-alpha) * outputValue + alpha * sensorValue; 
   //Serial.println(outputValue);
-  SICK_debug.data = outputValue; 
-  SICK_counter.publish(&SICK_debug); 
+  SICK_debug.data = (int)millis(); 
+  SICK_distance.publish(&SICK_debug); 
 
-  if(outputValue < 700 && t0_set_flag == false){
+  if(outputValue < 900 && t0_set_flag == false){
     t0 = millis(); 
     t0_set_flag = true; 
     //SICK_msg.data = (float)t0;
-    //SICK_sensor.publish(&SICK_msg);
+    //SICK_time.publish(&SICK_msg);
   }
-  if(outputValue > 800 && t0_set_flag == true){
+  if(outputValue > 900 && t0_set_flag == true && millis()-t0 > 1500){
     t1 = millis(); 
     t0_set_flag = false; 
     SICK_msg.data = (float)t1-t0;
-    SICK_sensor.publish(&SICK_msg);
+    SICK_time.publish(&SICK_msg);
   }
   
   nh.spinOnce(); 
-  delay(2); 
+  delay(3); 
    
 }
