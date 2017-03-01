1e9006a03b8e57598e79870016095737b2cd48dd
imoe <lym602@126.com>
2017-03-01 23:26:01 +0300

    SMOOTH_PARAM added

--- adc_mega/adc_mega.ino
+++ adc_mega/adc_mega.ino
@@ -6,6 +6,7 @@
 #define ERR_THR 1500
 #define VALVE_THR 800
 #define DELAY_MILI 2
+#define SMOOTH_PARAM 20
 
 ros::NodeHandle nh;  
 std_msgs::Int32 SICK_debug; 
@@ -15,6 +16,9 @@ ros::Publisher SICK_realdistance("SICK_realdistance", &SICK_rdistance);
 
 int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;   
+int outputValue = 0; 
+int lastValues[20]; 
+int counter = 0; 
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -29,10 +33,25 @@ void loop() {
   sensorValue = analogRead(sensorPin);
  
   //Serial.println(outputValue);
+
+  if(counter < SMOOTH_PARAM){
+    lastValues[counter] = sensorValue; 
+    outputValue = sensorValue; 
+   }else{
+    lastValues[counter%SMOOTH_PARAM] = sensorValue; 
+    outputValue = 0;
+    for(int i = 0; i < SMOOTH_PARAM; i++){
+      outputValue += lastValues[i]; 
+    }
+    outputValue /= SMOOTH_PARAM; 
+   }
+  counter++; 
+  
+if(counter == 2 * SMOOTH_PARAM){ counter = SMOOTH_PARAM; }
   
   // 5cm to 100cm, 4mA-20mA, 200-1023.
   //8.895 for 1 cm
-  SICK_debug.data = (int)sensorValue; 
+  SICK_debug.data = (int)outputValue; 
   SICK_rdistance.data = ((int)sensorValue-200)/8.895 + 5.0;
   SICK_distance.publish(&SICK_debug); 
   SICK_realdistance.publish(&SICK_rdistance); 
