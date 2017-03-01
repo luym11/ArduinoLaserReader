2bc1fb2f2c5c1cd69ad7040d49eb466d220201b3
imoe <lym602@126.com>
2017-03-01 23:14:16 +0300

    help the push button

--- adc_mega/adc_mega.ino
+++ adc_mega/adc_mega.ino
@@ -3,71 +3,39 @@
 #include <std_msgs/Int32.h>
 #include <ros/time.h>
 
-#define ERR_THR 800
+#define ERR_THR 1500
 #define VALVE_THR 800
-#define DELAY_MILI 3
+#define DELAY_MILI 2
 
-ros::NodeHandle nh; 
-std_msgs::Float32 SICK_msg; 
-ros::Publisher SICK_time("SICK_time", &SICK_msg); 
+ros::NodeHandle nh;  
 std_msgs::Int32 SICK_debug; 
+std_msgs::Float32 SICK_rdistance; 
 ros::Publisher SICK_distance("SICK_distance", &SICK_debug); 
-
+ros::Publisher SICK_realdistance("SICK_realdistance", &SICK_rdistance);
 
 int sensorPin = A0;    // select the input pin for the potentiometer
-int sensorValue = 0;  // variable to store the value coming from the sensor
-int outputValue = 0; 
-int lastValues[10]; 
-int counter = 0; 
-float alpha = 0.2; 
-long t0 = 0; 
-long t1 = 0;
-bool t0_set_flag = false;  
+int sensorValue = 0;   
 
 void setup() {
   // declare the ledPin as an OUTPUT:
   nh.initNode(); 
-  nh.advertise(SICK_time); 
   nh.advertise(SICK_distance);
+  nh.advertise(SICK_realdistance);
   Serial.begin(57600);
 }
 
 void loop() {
   
   sensorValue = analogRead(sensorPin);
-  
-  if(counter < 10){
-    lastValues[counter] = sensorValue; 
-    outputValue = sensorValue; 
-   }else{
-    lastValues[counter%10] = sensorValue; 
-    outputValue = 0;
-    for(int i = 0; i < 10; i++){
-      outputValue += lastValues[i]; 
-    }
-    outputValue /= 10; 
-   }
-  counter++; 
-  
-  if(counter == 20){ counter = 10; }
-  
-  
+ 
   //Serial.println(outputValue);
-  SICK_debug.data = (int)outputValue; 
+  
+  // 5cm to 100cm, 4mA-20mA, 200-1023.
+  //8.895 for 1 cm
+  SICK_debug.data = (int)sensorValue; 
+  SICK_rdistance.data = ((int)sensorValue-200)/8.895 + 5.0;
   SICK_distance.publish(&SICK_debug); 
-
-  if(outputValue < VALVE_THR && t0_set_flag == false){
-    t0 = millis(); 
-    t0_set_flag = true; 
-    //SICK_msg.data = (float)t0;
-    //SICK_time.publish(&SICK_msg);
-  }
-  if(outputValue > VALVE_THR && t0_set_flag == true && millis()-t0 > ERR_THR){
-    t1 = millis(); 
-    t0_set_flag = false; 
-    SICK_msg.data = (float)t1-t0;
-    SICK_time.publish(&SICK_msg);
-  }
+  SICK_realdistance.publish(&SICK_rdistance); 
   
   nh.spinOnce(); 
   delay(DELAY_MILI); 
